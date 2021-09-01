533a0bc0f812f578ab50a012676ca09c1acc0608
Sean Ma <ssma@stanford.edu>
2021-09-01 15:09:06 -0700

    initial commit

---
+++ cyberpunkweaponprops.js
@@ -0,0 +1,9 @@
+Hooks.on('ready', () => {
+	CONFIG.DND5E.weaponProperties['automatic'] = 'Automatic';
+  	CONFIG.DND5E.weaponProperties['blast'] = 'Blast';
+	CONFIG.DND5E.weaponProperties['burstfire'] = 'Burst-Fire';
+	CONFIG.DND5E.weaponProperties['marine'] = 'Marine';
+	CONFIG.DND5E.weaponProperties['massive'] = 'Massive';
+	CONFIG.DND5E.weaponProperties['scatter'] = 'Scatter';
+	CONFIG.DND5E.weaponProperties['scoped'] = 'Scoped';
+});
