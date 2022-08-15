8a758b0f3969ea88f319a5bd2f14386c000be43e
Sean Ma <ssma@stanford.edu>
2022-08-14 23:45:12 -0700

    updating weapon props

--- scripts/cyberpunk_weapon_props.js
+++ scripts/cyberpunk_weapon_props.js
@@ -3,7 +3,7 @@ Hooks.on('ready', () => {
 	CONFIG.DND5E.weaponProperties['foregrip'] = 'Foregrip';
 	CONFIG.DND5E.weaponProperties['mounted'] = 'Mounted';
 	CONFIG.DND5E.weaponProperties['scatter'] = 'Scatter';
-	CONFIG.DND5e.weaponProperties['strreq'] = 'Strength';
+	CONFIG.DND5E.weaponProperties['strreq'] = 'Strength';
 	CONFIG.DND5E.weaponProperties['sighted'] = 'Sighted';
 	CONFIG.DND5E.weaponProperties['actionreload'] = 'Reload (Action)';
 });
